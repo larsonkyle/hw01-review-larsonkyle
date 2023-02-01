d1742bdbd89b3e60c054c04b8decb287c3216274
larsonkyle <klarson22@saddleback.edu>
2023-02-01 00:44:42 -0800

    coded circle.cpp source code

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,1 +1,17 @@
-// Place function definition source code here
+#include <cmath>
+
+double distance (double x1, double y1, double x2, double y2) {
+	return std::sqrt(std::pow((x2 - x1), 2) + std::pow((y2 - y1), 2));
+}
+
+double radius (double x1, double y1, double x2, double y2) {
+	return distance(x1, y1, x2, y2);
+}
+
+double circumference (double r) {
+	return 2 * 3.1416 * r;
+}
+
+double area (double r) {
+	return (std::pow(r,2)) * 3.1416;
+}
