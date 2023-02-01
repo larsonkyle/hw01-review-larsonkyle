cd16d468030a7c87ae203ba8931a7b4d51bc04aa
larsonkyle <klarson22@saddleback.edu>
2023-02-01 00:46:38 -0800

    coded circle.h header file

--- src/circle.h
+++ src/circle.h
@@ -1,1 +1,12 @@
-// Put function header file here
+#ifndef _CIRCLE_H_
+#define _CIRCLE_H_
+
+double distance (double x1, double y1, double x2, double y2);
+
+double radius (double x1, double y1, double x2, double y2);
+
+double circumference (double r);
+
+double area (double r);
+
+#endif
