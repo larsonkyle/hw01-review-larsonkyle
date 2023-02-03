24910bb6764969b82f069286ce8318a7a155d8f8
larsonkyle <klarson22@saddleback.edu>
2023-02-02 17:18:32 -0800

    fixed std:: compile errors

--- src/circle.cpp
+++ src/circle.cpp
@@ -1,7 +1,7 @@
 
 
 double distance (double x1, double y1, double x2, double y2) {
-	return std::sqrt(std::pow((x2 - x1), 2) + std::pow((y2 - y1), 2));
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
 }
 
 double radius (double x1, double y1, double x2, double y2) {
@@ -13,5 +13,5 @@ double circumference (double r) {
 }
 
 double area (double r) {
-	return (std::pow(r,2)) * 3.1416;
+	return (pow(r,2)) * 3.1416;
 }
