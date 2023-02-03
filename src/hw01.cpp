9d6b1332897168bd12a2ba83bd607b2ccedcc75a
larsonkyle <klarson22@saddleback.edu>
2023-02-02 16:36:34 -0800

    pushed hw01.cpp

--- src/hw01.cpp
+++ src/hw01.cpp
@@ -1,5 +1,16 @@
-// You may use this file as a main function to test your code before you submit
+#include "circle.h"
+#include <iostream>
 
+int main() {
+	double x1 = 65.23,
+	       x2 = 54.54;
 
+	double y1 = 3.86,
+	       y2 = 5;
 
-
+	std::cout << "Distance: " << distance(x1,y1,x2,y2) << std::endl;
+	std::cout << "Radius: "   << radius(x1,y1,x2,y2) << std::endl;
+	std::cout << "Circumference: " << circumference(radius(x1,y1,x2,y2)) << std::endl;
+	std::cout << "Area: " << area(radius(x1,y1,x2,y2)) << std::endl;
+	return 0;
+}
